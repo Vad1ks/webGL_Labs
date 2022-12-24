7a84156a3fb1a4096fae76bdb49315512569592a
vadym sosnovenko <vadiksosnovenko@gmail.com>
2022-12-24 19:28:27 +0200

    changed surface mode to triangle_fan

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function Model(name) {
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
    
-        gl.drawArrays(gl.LINE_STRIP, 0, this.count);
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, this.count);
     }
 }
 
